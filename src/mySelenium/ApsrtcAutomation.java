cb154b7744cebdaca2ab7eecd430a527e01442f6
NagKothapalli <nag022@gmail.com>
2023-12-30 23:59:24 +0530

    Dec29th code pushed

--- src/mySelenium/ApsrtcAutomation.java
+++ src/mySelenium/ApsrtcAutomation.java
@@ -1,22 +1,29 @@
 package mySelenium;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Set;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
 
 public class ApsrtcAutomation 
 {
-	ChromeDriver cdriver; // null // instance variable
+	WebDriver cdriver; // null // instance variable
 	String sname = "Ram";
 	Actions actions;
 	public ApsrtcAutomation()
 	{
+		System.setProperty("webdriver.chrome.driver", "D:\\Softwares\\JarFiles\\chromedriver-win64-120-2\\chromedriver-win64\\chromedriver.exe");
+		//ChromeOptions coptions = new ChromeOptions();
+		//coptions.setBrowserVersion("Stable");
 		cdriver = new ChromeDriver(); // abcd12345
 		actions = new Actions(cdriver);
 		cdriver.manage().window().maximize();
@@ -36,7 +43,7 @@ public class ApsrtcAutomation
 	public void bookBusTicket() throws InterruptedException
 	{
 		System.out.println("Test Case : Book Bus Ticket");
-		cdriver.findElement(By.xpath("//input[@name='source']")).sendKeys("HYDERABAD");
+		cdriver.findElement(By.xpath("//input[@name='source']")).sendKeys("HYDERABAD"); //Hardcoded
 		Actions actions = new Actions(cdriver);
 		//Thread.sleep(1000);
 		//actions.sendKeys(Keys.TAB).build().perform();
@@ -74,9 +81,28 @@ public class ApsrtcAutomation
 		actions.moveToElement(fromCity).click().sendKeys("KURNOOL").pause(Duration.ofSeconds(1)).sendKeys(Keys.ENTER).doubleClick().contextClick().build().perform();
 	}
 	
-	public void handleMultipleWindows()
+	//org.openqa.selenium.NoSuchSessionException: Session ID is null. Using WebDriver after calling quit()?
+	
+	@Test
+	public void handleMultipleWindows() throws InterruptedException
 	{
+		System.out.println("First window title :" + cdriver.getTitle());
 		cdriver.findElement(By.xpath("//a[@title='TimeTable / Track']")).click();
+		cdriver.findElement(By.xpath("//a[text()='All services Time Table & Tracking']")).click();
+		Set<String> windows = cdriver.getWindowHandles();
+		ArrayList<String>  allwindows = new ArrayList<String>(windows);
+		int count = allwindows.size();
+		System.out.println(allwindows.get(0)); //efds53fdsf
+		System.out.println(allwindows.get(1)); //hjh565gjhgfdg
+		cdriver.switchTo().window(allwindows.get(1));
+		Thread.sleep(2000);
+		String title2 = cdriver.getTitle();
+		System.out.println("Second window title :" + title2);
+		cdriver.close();		
+		cdriver.switchTo().window(allwindows.get(0));
+		Thread.sleep(2000);
+		cdriver.findElement(By.xpath("//a[text()='Home']")).click();
+		cdriver.quit();
 	}
 	
 	
